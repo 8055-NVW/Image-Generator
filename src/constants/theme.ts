d8855dcc9f9133518c431daf2fb9e2043c263701
Vivian Lopes <Viv@Evys-MacBook-Pro.local>
2025-03-24 12:24:01 +0000

    pushing any additional change

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,24 +1,24 @@
-const theme = {
-    colors: {
-      blue50: "#5596f6",
-      blue60: "#2a7bf4",
-      blue70: "#2a5ca7",
-      darkblue30: "#7a8faf",
-      darkblue50: "#002966",
-      darkblue70: "#00193d",
-      green50: "#9fd966",
-      green60: "#74bd29",
-      green70: "#6a9440",
-      grey40: "#ccc",
-      grey70: "#666",
-      grey80: "#333",
-      red50: "#e85e23",
-      red60: "#cb4c15",
-      red70: "#9f370a",
-      yellow50: "#fcd33f",
-      yellow60: "#e6b60d",
-      yellow70: "#cf8e10",
-    },
-  }
+// const theme = {
+//     colors: {
+//       blue50: "#5596f6",
+//       blue60: "#2a7bf4",
+//       blue70: "#2a5ca7",
+//       darkblue30: "#7a8faf",
+//       darkblue50: "#002966",
+//       darkblue70: "#00193d",
+//       green50: "#9fd966",
+//       green60: "#74bd29",
+//       green70: "#6a9440",
+//       grey40: "#ccc",
+//       grey70: "#666",
+//       grey80: "#333",
+//       red50: "#e85e23",
+//       red60: "#cb4c15",
+//       red70: "#9f370a",
+//       yellow50: "#fcd33f",
+//       yellow60: "#e6b60d",
+//       yellow70: "#cf8e10",
+//     },
+//   }
   
-  export default theme
+//   export default theme
